bf9f637d08a20eb056a6d22bef20f8308ea52ec5
Luis Carlos Galvão <dev01@mowahtecnologia.com>
2020-07-03 15:24:06 -0400

    feat: create base server to connect to mongodb and run application

---
+++ src/main/index.ts
@@ -0,0 +1,18 @@
+import express from 'express'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+import env from './config/env'
+import Mongo from 'mongodb'
+import pointRouter from './routes/points'
+import { MongoHelper } from '../repositories/mongo-helper'
+const app:express.Application = express()
+
+app.use(bodyParser.json())
+app.use(pointRouter)
+MongoHelper.connect(env.url).then(() => {
+  app.listen(3000, () => {
+    console.log('running')
+  })
+}).catch(err => {
+  console.log(err)
+})
