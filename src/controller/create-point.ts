c1f2e4915c5675cdf0d0bac5b9216256a2a535a5
Luis Carlos Galvão <dev01@mowahtecnologia.com>
2020-07-06 11:47:38 -0400

    feat: ensure CreateInterestPoint controller calls validation

--- src/controller/create-point.ts
+++ src/controller/create-point.ts
@@ -1,16 +1,24 @@
 import { CreatePointOfInterest } from '../domain/use-case/create-point'
 import { Request, Response } from 'express'
+import Validation from '../validation/protocols/validation'
 export default class CreateInterestPoint {
-  constructor (private createPointOfInterest:CreatePointOfInterest) {}
+  constructor (
+    private createPointOfInterest:CreatePointOfInterest,
+    private validator: Validation
+  ) {}
 
   async handle (request: Request, response: Response): Promise<Response> {
     try {
+      const validationResponse = this.validator.validate(request.body)
+
       const { name, coordinateX, coordinateY }:
       {name:string, coordinateX:number, coordinateY: number} = request.body
-
       const coordenate = {
         x: coordinateX,
         y: coordinateY
+      }
+      if (validationResponse.error) {
+        return response.status(400).json(`Invalid field ${validationResponse.field}`)
       }
       const createdPoint = await this.createPointOfInterest.add({ name, coordenate })
       response.status(200)
